fcc3b050601990f4a97554b2c338347b190cf0a8
Victor <victor.lima.silva@hotmail.com>
2024-02-18 01:31:52 -0300

    -Upgrade das funcionalidades e validações.

--- WebApplication6/Controllers/HomeController.cs
+++ WebApplication6/Controllers/HomeController.cs
@@ -24,15 +24,76 @@ namespace WebApplication6.Controllers
         }
 
         [HttpPost]
-        public IActionResult Acao(Cliente cliente)
+        public IActionResult Acao(Cliente cliente, string submit)
         {
-            if (ModelState.IsValid)
+            if (submit == "Enviar")
             {
+
+                var clienteExistente = _contexto.cadastro_clientes.FirstOrDefault(c => c.Cpf == cliente.Cpf);
+                if (clienteExistente != null)
+                {
+                    ViewData["message"] = "CPF já existe na base de dados!";
+                    return View("Cadastro", cliente);
+                }
+
                 _contexto.cadastro_clientes.Add(cliente);
                 _contexto.SaveChanges();
-                return RedirectToAction("Index");
+                ViewData["message"] = "Cliente cadastrado com sucesso!";
+                return View("Cadastro", cliente);
+
+
             }
+            else if (submit == "Alterar")
+            {
+                if (ModelState.IsValid)
+                {
+                    Cliente clienteParaAtualizar = _contexto.cadastro_clientes.Find(cliente.Id);
+                    if (clienteParaAtualizar != null)
+                    {
+                        clienteParaAtualizar.Nome = cliente.Nome;
+                        clienteParaAtualizar.Sobrenome = cliente.Sobrenome;
+                        _contexto.cadastro_clientes.Update(clienteParaAtualizar);
+                        _contexto.SaveChanges();
+                        ViewData["message"] = "Cliente Alterado com sucesso!";
+                        return View("Cadastro", cliente);
+                    }
+                    else
+                    {
+                        ViewData["message"] = "Registro inexistente!";
+                        return View("Cadastro", cliente);
+                    }
+
+                }
+            }
+            else if (submit == "Deletar")
+            {
+                if (ModelState.IsValid)
+                {
+                    Cliente clienteParaDeletar = _contexto.cadastro_clientes.Find(cliente.Id);
+
+                    if (clienteParaDeletar != null)
+                    {
+                        _contexto.cadastro_clientes.Remove(clienteParaDeletar);
+                        _contexto.SaveChanges();
+                        ViewData["message"] = "Cliente Deletado com sucesso!";
+                        return View("Cadastro", cliente);
+                    }
+                    else
+                    {
+                        ViewData["message"] = "Registro inexistente!";
+                        return View("Cadastro", cliente);
+                    }
+                }
+            }
+            ViewData["message"] = "Nenhuma ação registrada";
             return View("Cadastro", cliente);
+        }
+        [HttpGet]
+        public IActionResult Consultar(int idCliente)
+        {
+            var clienteConsultado = _contexto.cadastro_clientes.FirstOrDefault(c => c.Id == idCliente);
+            ViewBag.ClienteConsultado = clienteConsultado;
+            return View("Cadastro");
         }
     }
 }
--- WebApplication6/Models/Cliente.cs
+++ WebApplication6/Models/Cliente.cs
@@ -1,10 +1,15 @@
-﻿namespace WebApplication6.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication6.Models
 {
     public class Cliente
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "O campo Nome é obrigatório!")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo Sobrenome é obrigatório!")]
         public string Sobrenome { get; set; }
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "O CPF deve conter exatamente 11 dígitos.")]
         public string Cpf { get; set; }
     }
 }
